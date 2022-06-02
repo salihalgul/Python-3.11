dd9dcc9cfc8609258595c8ddaa2e428bf9edaf66
Salih Algul <salihalgul@gmail.com>
2022-06-02 13:15:46 +0300

    yeni

--- 2022-bahar-sabah-master/pratikler/fonksiyon.py
+++ 2022-bahar-sabah-master/pratikler/fonksiyon.py
@@ -16,7 +16,22 @@
 
 #     print(f(4))
 
-def square(x):
-    res = x * x
-    return res
-square(3)
+# def square(x):
+#     res = x * x
+#     return res
+# square(3)
+
+# def f(x, y = True):
+
+#   if x % 2 == 0:
+
+#     y = False
+
+#   return y
+#  f(7,True)
+
+a = 2
+def f(x):
+    x = 4
+    return x
+print(f(a))
