4c5bc27411fa3793387b75d6622c84fe31feb52e
Salih Algul <salihalgul@gmail.com>
2022-06-02 11:44:24 +0300

    yeni

---
+++ 2022-bahar-sabah-master/pratikler/fonksiyon.py
@@ -0,0 +1,22 @@
+# x = 7
+
+# def f(x):
+
+#   res = 5
+
+#   res *= res
+#   if x % 2 == 0:
+
+#     print("Sonuc: ", res)
+
+#     return res 
+#   else:
+
+#     print("Sonuc: ", res)
+
+#     print(f(4))
+
+def square(x):
+    res = x * x
+    return res
+square(3)
---
+++ 2022-bahar-sabah-master/pratikler/zip.py
@@ -0,0 +1,5 @@
+notlar = [90,80,72]
+ogrenciler = ["ogrenci_1", "ogrenci_2", "ogrenci_3"]
+for s, g in zip(ogrenciler, notlar):
+    print(s, g)
+    
