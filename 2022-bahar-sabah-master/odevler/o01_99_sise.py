f7855142db140747b19ec71c5bd3d8dfc789d9c4
Salih Algul <83082628+salihalgul@users.noreply.github.com>
2022-06-05 11:14:45 +0300

    Update o01_99_sise.py

--- 2022-bahar-sabah-master/odevler/o01_99_sise.py
+++ 2022-bahar-sabah-master/odevler/o01_99_sise.py
@@ -25,3 +25,16 @@ yazdırılıp döngü kırılır.
 1 bottles of beer on the wall. 1 bottles of beer. Take one down, pass it around, 0 bottle of beer on the wall.
 There are no more bottles of beer on the wall!
 """
+for i in range(99, 0, -1): for # döngüsü içerisinde belirlenen i verilerinin 99 dan 1 e kadar geriye doğru sayması sağlanır.
+    if i > 2: # i verilerinin 2 den büyük olduğu durumlarda if komutundaki sorgular gerçekleşir ve her seferinde aksi (False) olmadıkça döngü başa döner.
+        beer = f'{i} bottles of beer'  # beer ve beer_1 eşitlikleri kullanılarak sorgumuza sonuçta kullanabileceği bir veri seti oluşturulur.
+        beer_1 = f'{i-1} bottles of beer'
+    elif i == 2:               # Döngü içerisindeki tanımlı i sayıları 99'dan geriye sayarken 2 ye geldiğinde if ten sonra gelen ilk elif komut bloğu gerçekleşir.
+        beer = f'{i} bottles of beer'     # i nin 2 ye eşit olduğu durumda beer ve beer_1 olarak belirttiğimiz sonuç ekranında kullanılacak olan tanımlamalar için 
+        beer_1 = f'{i-1} bottle of beer'    # yeni eşitlik yani ifadelerin nasıl olacağı belirtilmiştir.
+    else:
+        beer = f'{i} bottle of beer'     # Tüm komut satırlarında döngü içinde sorgulanan bloklar False dönerse yani bu örnekte i ile belirtilen aralıkta dönen sayılar
+        beer_1 = f'{i-1} bottle of beer'  # 1 e eşit olduğunda yeni belirlenen beer ve beer_1 deki eşitlikler sonuca yansıyacaktır.
+    print(f'{beer} on the wall. {beer}. Take one down, pass it around, {beer_1} on the wall.')  # Bu ifade ile 99 dan 1 e kadar gerçekleşen sayma işleminde 
+                                          # ve döngü içerisindeki sayının karşılığı olan sorgulardaki beer ve beer_1 ifadeleri alt alta sonuç ekranında yazdırılır.
+print('There are no more bottles of beer on the wall!') # Tüm satırlar ekranda yazdırıldıktan sonra ve döngü kırıldığında ise son satır olarak ekranda belirecek ifadedir.
