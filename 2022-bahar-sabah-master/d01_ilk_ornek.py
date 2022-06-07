936a91e27951351b90417b4cfe2984069080660a
Salih Algul <salihalgul@gmail.com>
2022-06-07 11:00:54 +0300

    ok

---
+++ 2022-bahar-sabah-master/d01_ilk_ornek.py
@@ -0,0 +1,24 @@
+print("Hello World")
+
+# print("Merhaba") print("Dunya")
+
+"""
+ben bu paragrafi yorum altina almak istiyorum.
+o zaman ne yapmam gerekli?
+"""
+
+print("Sonrasında yazdığım çıkmayacak")  # Bu da baska bir yorum
+
+
+def bu_ornek_bir_fonskiyondur():
+    print('Baba beni okula gonder')
+    print('biz hala fonksiyonun icindeyiz')
+
+
+bu_ornek_bir_fonskiyondur()
+print('burasi ise fonksiyonun disindadir')
+
+
+
+
+
