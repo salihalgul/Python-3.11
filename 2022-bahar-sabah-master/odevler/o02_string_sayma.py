eb0a08b6c24a5657a449913047acf9e2f5c97936
Salih Algul <salihalgul@gmail.com>
2022-06-08 20:15:29 +0300

    bazı değişiiklikler oldu

--- 2022-bahar-sabah-master/odevler/o02_string_sayma.py
+++ 2022-bahar-sabah-master/odevler/o02_string_sayma.py
@@ -29,3 +29,11 @@ Ne sen bunun farkındasın ne polis farkında
 Ben bir ceviz ağacıyım Gülhane Parkı’nda
 Ne sen bunun farkındasın ne de polis farkında
 """
+str = "Başım köpük köpük bulut İçim dışım deniz Ben bir ceviz ağacıyım Gülhane Parkı’nda Budak budak,şerham şerham ihtiyar bir ceviz Ne sen bunun farkındasın ne polis farkında Ben bir ceviz ağacıyım Gülhane Parkı’nda Ne sen bunun farkındasın ne de polis farkında"
+sesli = ["A", "a", "E", "e", "I", "ı", "İ", "i", "O", "o", "Ö", "ö", "U", "u", "Ü", "ü"]
+# result = str.split(" ")
+i = 0
+sesli_adedi = ["A: "]
+for i in sesli:
+    sesli_adedi = input("Sesli:")
+    print(str.count(i))
