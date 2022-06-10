007904aa597e9287e17909a87b279baff5750e39
Salih Algül <salihalgul@gmail.com>
2022-06-10 09:51:09 +0300

    new changes

---
+++ 2022-bahar-sabah-master/pratikler/operators.py
@@ -0,0 +1,48 @@
+# x, y, z = 2, 5, 10
+# numbers = 1, 5, 7, 10, 6
+#  a = int(input("1. sayı: "))
+#  b = int(input("2. sayı: "))
+#  result = (a * b) - (x+y+z)
+
+# result = y // x
+
+# toplam = (x+y+z)
+# result = toplam%3
+
+# result = y ** x
+
+# x, *y, z = numbers
+
+# numbers = 1, 5, 7, 10, 6
+# result = z ** 3
+
+# result = y[0] + y[1] + y[2]
+
+# print(result)
+
+# a = int(input("a: "))
+# b = int(input("b: "))
+
+# result = a > b
+# print(f"a: {a} b: {b} den büyüktür. {result}")
+
+# vize1 = float(input("1. vize: "))
+# vize2 = float(input("2. vize: "))
+# final = float(input("final notu: "))
+
+# ortalama = ((vize1 + vize2) /2 * 0.6) + (final * 0.4)
+
+# print(f"not ortalamanız: {ortalama} ve dersten geçme durumunuz: {ortalama >= 50}")
+
+# sayı = int(input("Bir sayı giriniz: "))
+
+# tekcift = (sayı % 2 == 0)
+
+# print(f"Girilen sayının çift olma durumu: {tekcift}")
+
+# sayı = int(input("Bir sayı giriniz: "))
+
+# pozitif = (sayı >= 0)
+
+# print(f"Sayının pozitif olma durumu: {pozitif}")
+
