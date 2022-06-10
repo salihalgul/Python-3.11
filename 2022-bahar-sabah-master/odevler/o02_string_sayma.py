14aa6ce328ec150666a78a4bd3c75b70b89406be
Salih Algül <salihalgul@gmail.com>
2022-06-10 22:44:47 +0300

    sert sessizler eklendi

--- 2022-bahar-sabah-master/odevler/o02_string_sayma.py
+++ 2022-bahar-sabah-master/odevler/o02_string_sayma.py
@@ -36,10 +36,9 @@ str = "Başım köpük köpük bulut İçim dışım deniz Ben bir ceviz ağacı
 # sesli_sayim = {i : str.count(i) for i in sesli}
 
 sesli_sayim = {i : str.count(i) for i in 'aeıioöuü'}
-
+sertsessiz_sayim = {mahmut : str.count(mahmut) for mahmut in 'çfthskpş'}
 
 print(sesli_sayim)
-
 
 mod_str = str.lower().strip()
 
@@ -47,4 +46,7 @@ mod_str = str.lower().strip()
 for i in 'aeıioöuü':
     print(f"{i} = {mod_str.count(i)}")
 
+print(f"Sert sessizler sözlüğü= {sertsessiz_sayim}")
+for mahmut in 'çfthskpş':
+    print(f"Sert sessiz {mahmut} harfi {mod_str.count(mahmut)} adettir.")
 
