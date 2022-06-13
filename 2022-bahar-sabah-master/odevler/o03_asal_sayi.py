be053b5e9e8f8900ae0afcefe695eeabfa50f1b2
Salih Algül <salihalgul@gmail.com>
2022-06-13 19:08:09 +0300

    new homework

---
+++ 2022-bahar-sabah-master/odevler/o03_asal_sayi.py
@@ -0,0 +1,18 @@
+"""
+Bu ödev için bir sayının asal olup olmadığını denetleyen bir fonksiyon yazacaksınız.
+Eğer sayı asal ise fonksiyon True, değilse False dönecek.
+Fonksiyon argüman olarak sadece sayıyı alacak.
+
+Sonrasında da bu fonksiyonu 1-50 arası bir döngüdeki bütün sayılar ile test edecek bir döngü yazılacak.
+Fonksiyon True dönerse bu sayı ekrana yazdırılacak.
+
+Size aşağıda Fonksiyon ve döngü için bir kalıp verildi. O kalıbın içerisinde kodunuzu yazınız.
+"""
+
+
+def is_prime(number):
+    # Bu sizin asal sayı kontrol edecek fonksiyonunuz olacak
+
+
+for i in range(1, 51):
+    # Burası da sizin fonksiyonu 1 ile 50 arasında çağıracağınız ve True dönerse ekrana yazdıracağınız yer olacak.
