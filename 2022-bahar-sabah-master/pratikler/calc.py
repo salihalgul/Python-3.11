a586267dd6b59c2417ef6a092296347ff3353ee8
Salih Algül <salihalgul@gmail.com>
2022-06-17 23:47:09 +0300

    ok

---
+++ 2022-bahar-sabah-master/pratikler/calc.py
@@ -0,0 +1,23 @@
+from re import A
+
+
+def calc(x,y,ops):
+    if ops not in "+-*/":
+        return "Only +-*/ !!!"
+    if ops == "+":
+        return(str(x) + " " + ops + " " + str(y) + " = " + str(x+y))
+    elif ops == "-":
+        return(str(x) + " " + ops + " " + str(y) + " = " + str(x-y))
+    elif ops == "*":
+        return(str(x) + " " + ops + " " + str(y) + " = " + str(x*y))
+    elif ops == "/":
+        return(str(x) + " " + ops + " " + str(y) + " = " + str(x/y))
+
+while True:
+
+    x = float(input("Please enter first number : "))
+    y = float(input("Please enter second number : "))
+    ops = input("Please choose calculation type : ")
+
+    print(calc(x,y,ops))
+
---
+++ 2022-bahar-sabah-master/pratikler/primes.py
@@ -0,0 +1,4 @@
+"""
+Soru: Girilen bir sayının asal olup olmadığını kontrol ediniz.
+** Asal sayı 1 ve kendisi hariç tam böleni olmayan sayılara denir.
+"""
