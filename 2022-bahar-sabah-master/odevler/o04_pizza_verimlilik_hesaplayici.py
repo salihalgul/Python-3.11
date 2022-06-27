fbbe28b0a7a66f7c16c3159466690d69afa9697d
Salih Algül <salihalgul@gmail.com>
2022-06-27 23:33:04 +0300

    ok

--- 2022-bahar-sabah-master/odevler/o04_pizza_verimlilik_hesaplayici.py
+++ 2022-bahar-sabah-master/odevler/o04_pizza_verimlilik_hesaplayici.py
@@ -16,3 +16,40 @@ Bu noktadan sonra bir büyük bir de küçük pizza için kullanıcıdan input a
 Sonrasında bu pizzaların alanlarını ve skorlarını hesaplayıp bunları ekrana yazdıracağız
 En son da hangi pizza daha avantajlı ise o pizzayı kazanan olarak belirteceğiz.
 """
+
+pi = 3.14
+
+def kucuk_pizza_performans():
+           
+    pizza_fiyati = int(input("Lütfen küçük pizza fiyatı giriniz: "))
+    
+    pizza_capi = int(input("Lütfen küçük pizza çapını giriniz: "))
+
+    pizza_alani = pi * (pizza_capi/2)**2
+    
+    performans = pizza_fiyati / pizza_alani
+    
+    return performans
+
+
+print("Küçük pizza performans puanı :", kucuk_pizza_performans())
+    
+def buyuk_pizza_performans():
+           
+    buyuk_pizza_fiyati = int(input("Lütfen büyük pizza fiyatı giriniz: "))
+    
+    buyuk_pizza_capi = int(input("Lütfen büyük pizza çapını giriniz: "))
+
+    buyuk_pizza_alani = pi * (buyuk_pizza_capi/2)**2
+    
+    buyuk_performans = buyuk_pizza_fiyati / buyuk_pizza_alani
+    
+    return buyuk_performans
+
+
+print("Büyük pizza performans puanı :", buyuk_pizza_performans())
+
+if buyuk_pizza_performans() > kucuk_pizza_performans():
+    print("Şampiyon Büyük Pizza")
+else:
+    print("Şampiyon Küçük Pizza")
